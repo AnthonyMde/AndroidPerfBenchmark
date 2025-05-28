b1582b587d3475b7ab488e42c06ffc7182c90cbf
AnthonyMde <anthony.mamode@gmail.com>
2025-05-28 16:13:49 +0200

    update perf test using coil

--- app/src/main/java/com/bc/androidperfbenchmark/component/ScrollingListPerf.kt
+++ app/src/main/java/com/bc/androidperfbenchmark/component/ScrollingListPerf.kt
@@ -1,6 +1,5 @@
 package com.bc.androidperfbenchmark.component
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -11,72 +10,54 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.bc.androidperfbenchmark.R
 import com.bc.androidperfbenchmark.anim.infiniteRotatingAnim
 
 @Composable
 fun ScrollingListPerf() {
-    val rotation by infiniteRotatingAnim()
-    val painters = remember {
-        pictureList
-    }.map { painterResource(it) }
-
-    LazyColumn {
-        items(300) {
-            RowListWithImages(rotation, painters)
-        }
+    val images = remember {
+        listOf(
+            R.drawable.image1,
+            R.drawable.image2,
+            R.drawable.image3
+        )
     }
-}
-
-@Composable
-fun RowListWithImages(rotation: Float, painters: List<Painter>) {
-    LazyRow {
-        items(painters) { painter ->
-            Box {
-                Image(
-                    painter,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(50.dp)
-                        .rotate(rotation)
-                )
+    
+    val rowImages = remember {
+        List(30) { index -> images[index % 3] }
+    }
+    
+    // Single shared rotation animation state for all images
+    val rotation by infiniteRotatingAnim()
+    
+    LazyColumn {
+        items(300) { _ ->
+            LazyRow {
+                items(rowImages) { imageRes ->
+                    RotatingImage(
+                        imageRes = imageRes,
+                        rotation = rotation
+                    )
+                }
             }
         }
     }
 }
 
-val pictureList = listOf(
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-    R.drawable.image1,
-    R.drawable.image2,
-    R.drawable.image3,
-)
+@Composable
+private fun RotatingImage(
+    imageRes: Int,
+    rotation: Float
+) {
+    Box(modifier = Modifier.size(50.dp)) {
+        AsyncImage(
+            model = imageRes,
+            contentDescription = null,
+            modifier = Modifier
+                .size(50.dp)
+                .rotate(rotation)
+        )
+    }
+}
