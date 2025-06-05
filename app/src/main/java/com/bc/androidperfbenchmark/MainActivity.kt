70726575b9b452ea81f18f80aba3fa7d84b4cf65
AnthonyMde <anthony.mamode@gmail.com>
2025-06-05 14:28:08 +0200

    Align android native code to cmp code

--- app/src/main/java/com/bc/androidperfbenchmark/MainActivity.kt
+++ app/src/main/java/com/bc/androidperfbenchmark/MainActivity.kt
@@ -4,12 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.bc.androidperfbenchmark.component.ScrollingListPerf
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.bc.androidperfbenchmark.ui.theme.AndroidPerfBenchmarkTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,10 +40,52 @@ class MainActivity : ComponentActivity() {
                         .fillMaxSize(),
                     horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    ScrollingListPerf()
-//                        ColumnPerf()
+                    LazyColumn {
+                        items(300) {
+                            TestRow()
+                        }
+                    }
                 }
             }
+        }
+    }
+
+    @Composable
+    fun TestRow() {
+        val infiniteTransition = rememberInfiniteTransition()
+        val angle by infiniteTransition.animateFloat(
+            initialValue = 0f,
+            targetValue = 360f,
+            animationSpec = infiniteRepeatable(
+                animation = tween(durationMillis = 1000, easing = LinearEasing)
+            )
+        )
+
+        LazyRow {
+            items(repeatedPictureList) { drawable ->
+                Box(modifier = Modifier.size(50.dp)) {
+                    AsyncImage(
+                        model = drawable,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(50.dp)
+                            .clip(RoundedCornerShape(8.dp))
+                            .rotate(angle)
+                    )
+                }
+            }
+        }
+    }
+
+    val pictureList = listOf(
+        R.drawable.image1,
+        R.drawable.image2,
+        R.drawable.image3
+    )
+
+    val repeatedPictureList = buildList {
+        repeat(10) {
+            addAll(pictureList)
         }
     }
 }
